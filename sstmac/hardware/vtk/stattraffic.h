77295662a8040268fb27a83542be1a088507a742
Perrinel Mériadeg <meriadeg.perrinel@ng-analytics.com>
2020-04-23 15:17:47 +0200

    #1: #3: stattraffic compile with sst-macro in its skeleton format
    This class will be probably removed later

--- sstmac/hardware/vtk/stattraffic.h
+++ sstmac/hardware/vtk/stattraffic.h
@@ -47,14 +47,11 @@ Questions? Contact sst-macro-help@sandia.gov
 #include <cmath>
 #include <limits>
 
-#include <sst/core/sst_types.h>
-#include <sst/core/warnmacros.h>
+//#include <sst/core/sst_types.h>
+//#include <sst/core/warnmacros.h>
 
-#include <sst/core/statapi/statfieldinfo.h>
-#include <sst/core/statapi/statoutput.h>
-#include <sst/core/statapi/statbase.h>
-#include <sst/core/baseComponent.h>
-#include "statoutputexodus.h"
+#include <sstmac/common/stats/stat_collector.h>
+#include <sstmac/common/sstmac_config.h>
 
 namespace sstmac {
 namespace hw {
@@ -77,59 +74,57 @@ namespace hw {
         @tparam NumberBase A template for the basic numerical type of values
 */
 
-class TrafficStatistic : public SST::Statistics::Statistic<traffic_event>
+class TrafficStatistic : public SST::Statistics::MultiStatistic<traffic_event>
 {
+    using Parent=SST::Statistics::MultiStatistic<traffic_event>;
 public:
-  SST_ELI_REGISTER_STATISTIC(TrafficStatistic,traffic_event,
+  SST_ELI_DECLARE_STATISTIC_TEMPLATE(TrafficStatistic,
      "macro",
      "traffic_intensity",
      SST_ELI_ELEMENT_VERSION(1,0,0),
      "VTK Traffic visualisation",
-     "SST::Statistic<T>")
+     "Statistic<traffic_event>")
 
-    TrafficStatistic(SST::BaseComponent* comp, const std::string& statName, const std::string& statSubId, SST::Params& statParams)
-        : SST::Statistics::Statistic<traffic_event>(comp, statName, statSubId, statParams)
+    TrafficStatistic(SST::BaseComponent* comp, const std::string& name,
+                     const std::string& subName, SST::Params& params)
+        : SST::Statistics::MultiStatistic<traffic_event>(comp, name, subName, params)
     {
-        std::cout << "TrafficStatistic constructor: " << comp->getName() << std::endl;
+//        std::cout << "TrafficStatistic constructor: " << comp->getName() << std::endl;
 
         // Set the Name of this Statistic
-        this->setStatisticTypeName("Traffic");
+//        this->setStatisticTypeName("Traffic");
     }
 
-    ~TrafficStatistic() {}
+//    ~TrafficStatistic() {}
 
-protected:    
-    /**
-        Present a new value to the class to be included in the statistics.
-        @param value New value to be presented
-    */
-    void addData_impl(traffic_event&& value) override
+//    /**
+//        Present a new value to the class to be included in the statistics.
+//        @param value New value to be presented
+//    */
+//    void addData_impl(traffic_event&& value) override
+//    {
+//        // USE uintptr_t as value to remove the dynamical parameters;
+
+//        auto tp = std::make_shared<traffic_event>();
+
+//        tp->time_ = value.time_;
+//        tp->id_ = value.id_;
+//        tp->port_ = value.port_;
+//        tp->color_ =value.color_;
+
+//        traffic_event_map_.insert({value.time_, tp});
+//    }
+
+    void addData_impl(traffic_event value) override
     {
-        // USE uintptr_t as value to remove the dynamical parameters;
-        //        Params &traffic_intensity_params = this->getDynamicParams();
 
+//         USE uintptr_t as value to remove the dynamical parameters;
         auto tp = std::make_shared<traffic_event>();
 
         tp->time_ = value.time_;
         tp->id_ = value.id_;
-        tp->p_ = value.p_;
-        tp->type_ =value.type_;
-
-        traffic_event_map_.insert({value.time_, tp});
-    }
-
-    void addData_impl(const traffic_event& value) override
-    {
-
-        // USE uintptr_t as value to remove the dynamical parameters;
-        //        Params &traffic_intensity_params = this->getDynamicParams();
-
-        auto tp = std::make_shared<traffic_event>();
-
-        tp->time_ = value.time_;
-        tp->id_ = value.id_;
-        tp->p_ = value.p_;
-        tp->type_ =value.type_;
+        tp->port_ = value.port_;
+        tp->color_ =value.color_;
 
         traffic_event_map_.insert({value.time_, tp});
     }
@@ -140,49 +135,49 @@ public:
         @return The sum of values presented to the class so far.
     */
 
-    void clearStatisticData() override
-    {
-        this->setCollectionCount(0);
-    }
+//    void clearStatisticData() override
+//    {
+//        this->setCollectionCount(0);
+//    }
 
-    void registerOutputFields(SST::Statistics::StatisticOutput* statOutput) override
+    void registerOutputFields(SST::Statistics::StatisticFieldsOutput* statOutput) override
     {
      // h_traffic_event = statOutput->registerField<NumberBase>("Traffic_event");
     }
 
-    void outputStatisticData(SST::Statistics::StatisticOutput* statOutput, bool UNUSED(EndOfSimFlag)) override
+    void outputStatisticFields(SST::Statistics::StatisticFieldsOutput* statOutput, bool  /*EndOfSimFlag*/) override
     {
-      auto statOutputExodus = dynamic_cast<sstmac::hw::StatisticOutputEXODUS *>(statOutput);
+//      auto statOutputExodus = dynamic_cast<sstmac::hw::StatisticOutputEXODUS *>(statOutput);
 
-      if(!statOutputExodus){
-        std::cout << "Impossible to cast the output of the stattraffic to an exodus output."<< std::endl;
-        return;
-      }
-      std::cout << "StatTraffic::outputStatisticData"<< std::endl;
-      std::cout << "reduce called on switch" << std::endl;
-      std::cout << " reduce " << this->traffic_event_map_.size()<<std::endl;
+//      if(!statOutputExodus){
+//        std::cout << "Impossible to cast the output of the stattraffic to an exodus output."<< std::endl;
+//        return;
+//      }
+//      std::cout << "StatTraffic::outputStatisticData"<< std::endl;
+//      std::cout << "reduce called on switch" << std::endl;
+//      std::cout << " reduce " << this->traffic_event_map_.size()<<std::endl;
 
-      for (auto it = this->traffic_event_map_.cbegin(); it != this->traffic_event_map_.cend(); ++it){
-        auto tp = std::make_shared<traffic_event>();
-        tp->time_ = it->first;
-        tp->id_ = it->second->id_;
-        tp->type_ = it->second->type_;
-        tp->p_ = it->second->p_;
+//      for (auto it = this->traffic_event_map_.cbegin(); it != this->traffic_event_map_.cend(); ++it){
+//        auto tp = std::make_shared<traffic_event>();
+//        tp->time_ = it->first;
+//        tp->id_ = it->second->id_;
+//        tp->type_ = it->second->type_;
+//        tp->p_ = it->second->p_;
 
-        // How to compute intensity ?
-        // Let's do it by making a +1/-1 depending of the previous scalar value
-        int previousIntensity = !this->traffic_progress_map_.empty() ?
-              this->traffic_progress_map_.crbegin()->second->intensity_ : 0;
-        tp->intensity_ = it->second->type_ ? previousIntensity - 1 : previousIntensity + 1;
+//        // How to compute intensity ?
+//        // Let's do it by making a +1/-1 depending of the previous scalar value
+//        int previousIntensity = !this->traffic_progress_map_.empty() ?
+//              this->traffic_progress_map_.crbegin()->second->intensity_ : 0;
+//        tp->intensity_ = it->second->type_ ? previousIntensity - 1 : previousIntensity + 1;
 
-        this->traffic_progress_map_.insert({it->first, tp});
-      }
+//        this->traffic_progress_map_.insert({it->first, tp});
+//      }
 
-      // need to do something with statouput
+//      // need to do something with statouput
 
-      for (auto& pair : this->traffic_progress_map_){
-        statOutputExodus->outputField(h_traffic_event, *(pair.second.get()));
-      }
+//      for (auto& pair : this->traffic_progress_map_){
+//        statOutputExodus->outputField(h_traffic_event, *(pair.second.get()));
+//      }
 
     }
 
